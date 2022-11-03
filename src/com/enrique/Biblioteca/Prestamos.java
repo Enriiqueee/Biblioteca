03d8f2d503ed4346cd2d040e65b7e3f6f2305d2e
enriiquee <94898081+Enriiqueee@users.noreply.github.com>
2022-11-03 18:22:32 +0100

    segundo commit

--- src/com/enrique/Biblioteca/Prestamos.java
+++ src/com/enrique/Biblioteca/Prestamos.java
@@ -13,5 +13,28 @@ public class Prestamos {
     public void setId(Integer id){
         this.id=id;
     }
-
+    public String getLibroprestado(){
+        return libroprestado;
+    }
+    public void setLibroprestado(String libroprestado){
+        this.libroprestado=libroprestado;
+    }
+    public String getUsuario(){
+        return usuario;
+    }
+    public void setUsuario(String usuario){
+        this.usuario=usuario;
+    }
+    public Integer getFechaprestamo(){
+        return fechaprestamo;
+    }
+    public void setFechaprestamo(Integer fechaprestamo){
+        this.fechaprestamo=fechaprestamo;
+    }
+    public Integer getFechadevolucion(){
+        return fechadevolucion;
+    }
+    public void setFechadevolucion(Integer fechaprestamo){
+        this.fechaprestamo=fechaprestamo;
+    }
 }
